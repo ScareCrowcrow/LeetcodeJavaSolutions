6c4dbda8129e694092787e69761cc840b2099344
wp.pc <1269086755@qq.com>
2020-04-03 13:18:09 +0800

    feat:2020-04-03 am

---
+++ src/p1_100/L0008_medium.java
@@ -0,0 +1,35 @@
+package p1_100;
+
+public class L0008_medium {
+    public static int myAtoi(String str) {
+        String string = str.trim();
+        char[] chars = string.toCharArray();
+        int index = 0;
+        if (chars.length == 0) return 0;
+        boolean sign = true;
+        if (chars[index] == '-'){
+            sign = false;
+            index++;
+        }
+        else if (chars[index] == '+'){
+            index++;
+        }else if (!Character.isDigit(chars[index])){
+            return 0;
+        }
+        int res = 0;
+        while (index < chars.length && Character.isDigit(chars[index])){
+            int digit = chars[index] - '0';
+            if (res > (Integer.MAX_VALUE - digit) / 10){
+                return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            res = res * 10 + digit;
+            index++;
+        }
+        return sign ? res : -res;
+    }
+
+    public static void main(String[] args) {
+        String str = "-91283472332";
+        System.out.println(myAtoi(str));
+    }
+}
