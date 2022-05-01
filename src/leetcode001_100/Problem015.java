1ac2a75f6a241b28a4bfb175a638eb25df722909
Peng Wang <pengwang@PengdeMacBook-Pro.local>
2022-05-01 22:31:37 +0800

    feat: 20220501_night

--- src/leetcode001_100/Problem015.java
+++ src/leetcode001_100/Problem015.java
@@ -6,7 +6,50 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Problem015 {
+    // https://leetcode-cn.com/problems/3sum/
+    // 在twoSum的基础上改造
     public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int N = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = N - 1; i > 1; i--) { // 三元组最后一个数，是arr[i]   之前....二元组 + arr[i]
+            if (i == N - 1 || nums[i] != nums[i + 1]) {
+                List<List<Integer>> nexts = twoSum(nums, i - 1, -nums[i]);
+                for (List<Integer> cur : nexts) {
+                    cur.add(nums[i]);
+                    ans.add(cur);
+                }
+            }
+        }
+        return ans;
+    }
+
+    // nums[0...end]这个范围上，有多少个不同二元组，相加==target，全返回
+    // {-1,5}     K = 4
+    // {1, 3}
+    public static List<List<Integer>> twoSum(int[] nums, int end, int target) {
+        int L = 0;
+        int R = end;
+        List<List<Integer>> ans = new ArrayList<>();
+        while (L < R) {
+            if (nums[L] + nums[R] > target) {
+                R--;
+            } else if (nums[L] + nums[R] < target) {
+                L++;
+            } else { // nums[L] + nums[R] == target
+                if (L == 0 || nums[L - 1] != nums[L]) {
+                    List<Integer> cur = new ArrayList<>();
+                    cur.add(nums[L]);
+                    cur.add(nums[R]);
+                    ans.add(cur);
+                }
+                L++;
+            }
+        }
+        return ans;
+    }
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
         ArrayList<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length <= 2) return res;
         int n = nums.length;
