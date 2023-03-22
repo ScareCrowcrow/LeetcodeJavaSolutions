23ec997090670373c4420e22474c057c70ad86c5
Peng Wang <pengwang@PengdeMacBook-Pro.local>
2023-03-22 22:55:40 +0800

    20230322

--- src/leetcode701_800/L739_DailyTemperatures_medium.java
+++ src/leetcode701_800/L739_DailyTemperatures_medium.java
@@ -6,42 +6,18 @@ import java.util.Stack;
 
 public class L739_DailyTemperatures_medium {
     // https://leetcode.cn/problems/daily-temperatures/
-    public int[] dailyTemperatures(int[] T) {
-        int[] res = new int[T.length];
-        Stack<Integer> stack = new Stack();
-        for (int i = 0; i < T.length; i++){
-            while (!stack.isEmpty() && T[i] > T[stack.peek()]){
-                res[stack.peek()] = i - stack.peek();
-                stack.pop();
+    // 单调栈
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> s = new Stack<>();
+        for(int i = n - 1; i >= 0; i--) {
+            while(!s.isEmpty() && temperatures[i] >= temperatures[s.peek()]) {
+                s.pop();
             }
-            stack.push(i);
+            res[i] = s.isEmpty() ? 0 : (s.peek() - i);
+            s.push(i);
         }
         return res;
-    }
-
-    // 单调栈
-    public static int[] dailyTemperatures2(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return new int[0];
-        }
-        int N = arr.length;
-        int[] ans = new int[N];
-        Stack<List<Integer>> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && arr[stack.peek().get(0)] < arr[i]) {
-                List<Integer> popIs = stack.pop();
-                for (Integer popi : popIs) {
-                    ans[popi] = i - popi;
-                }
-            }
-            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]) {
-                stack.peek().add(Integer.valueOf(i));
-            } else {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(i);
-                stack.push(list);
-            }
-        }
-        return ans;
     }
 }
